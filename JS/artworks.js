1c7615bb8f56c5c57a6d2dff36236d4017bfac8c
suppmen <suppmen@gmail.com>
2020-05-29 15:24:29 +0200

    working on dynamic part of single art page

--- JS/artworks.js
+++ JS/artworks.js
@@ -44,12 +44,33 @@ window.addEventListener("DOMContentLoaded", getData);
 /***** fetch Data *****/
 
 function getData() {
-    fetch(link1)
+
+    const urlParams = new URLSearchParams(window.location.search);
+//    console.log('URLSearchParams' + window.location);
+//    console.log('urlParams', urlParams);
+    const the_art_id = urlParams.get('art_id');
+    const search_term = urlParams.get('searchTerm');
+    const link2 = "https://mymmd.dk/Ane/wp-json/wp/v2/artwork"+the_art_id+"?per_page=100&_embed";
+    console.log(the_art_id, "IdTest");
+
+
+     if (the_art_id){
+        fetch(link2)
+        .then(function(response){
+            return response.json()
+        })
+        .then(showSingleArtPage)
+    }else{
+        fetch(link1)
         .then(function (response) {
             return response.json();
         })
         .then(showData);
+    }
 }
+
+
+
 
 function showData(artWorkArray) {
     console.log(artWorkArray, "artWorkArray");
@@ -78,12 +99,42 @@ function renderArtworkPage(ArtworkPageArray){
       copy.querySelector('.artwork-images').src = ArtworkPageArray._embedded["wp:featuredmedia"][0].source_url;
 
     copy.querySelector(".info").textContent = ArtworkPageArray.title.rendered;
+            const a = copy.querySelector('a');
 
 
+  if (a){
+                    a.href += ArtworkPageArray.id;
+
+        }
 
         document.querySelector(".artworkwrapper").appendChild(copy);
 
 
 
 }
+
+
+function showSingleArtPage(art){
+        console.log(art, "art");
+        console.log(window.location);
+        const template = document.querySelector("template").content;
+
+        const copy = template.cloneNode(true);
+
+        const divArtDescription = copy.querySelector('#art-description');
+        console.log(divArtDescription, "div");
+
+
+//    if(art.long_description.length> 1){
+//        divArtDescription.innerHTML = art.content.rendered;
+
+    copy.querySelector('.artwork-images').src = ArtworkPageArray._embedded["wp:featuredmedia"][0].source_url;
+
+
+
+
+        document.querySelector("single-art").appendChild(copy);
+
+    };
+
 
