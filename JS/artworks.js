2ed42cd68a69ccc0b306ce07ce7ad580bd653d8c
suppmen <suppmen@gmail.com>
2020-06-02 11:55:21 +0200

    js and about page fixes

--- JS/artworks.js
+++ JS/artworks.js
@@ -1,5 +1,3 @@
-
-
 const btnMenu = document.querySelector(".menu-btn");
 const btnExit = document.querySelector(".exit-btn");
 const headerUl = document.getElementById("menu");
@@ -47,22 +45,22 @@ function getData() {
 
     const urlParams = new URLSearchParams(window.location.search);
     const the_art_id = urlParams.get('art_id');
-    const link2 = "https://mymmd.dk/Ane/wp-json/wp/v2/artwork/"+the_art_id+"?per_page=100&_embed";
+    const link2 = "https://mymmd.dk/Ane/wp-json/wp/v2/artwork/" + the_art_id + "?per_page=100&_embed";
     console.log(the_art_id, "IdTest");
 
 
-     if (the_art_id){
+    if (the_art_id) {
         fetch(link2)
-        .then(function(response){
-            return response.json()
-        })
-        .then(showSingleArtPage)
-    }else{
+            .then(function (response) {
+                return response.json()
+            })
+            .then(showSingleArtPage)
+    } else {
         fetch(link1)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(showData);
+            .then(function (response) {
+                return response.json();
+            })
+            .then(showData);
     }
 }
 
@@ -81,45 +79,45 @@ function showData(artWorkArray) {
 
 
     });
-  showSlides();
+    showSlides();
 
 }
 
 
-function renderArtworkPage(ArtworkPageArray){
+function renderArtworkPage(ArtworkPageArray) {
 
     const template = document.querySelector(".artwork-page").content;
 
-        const copy = template.cloneNode(true);
+    const copy = template.cloneNode(true);
 
 
-      copy.querySelector('.artwork-images').src = ArtworkPageArray._embedded["wp:featuredmedia"][0].source_url;
+    copy.querySelector('.artwork-images').src = ArtworkPageArray._embedded["wp:featuredmedia"][0].source_url;
 
     copy.querySelector(".info").textContent = ArtworkPageArray.title.rendered;
-            const a = copy.querySelector('a');
+    const a = copy.querySelector('a');
 
 
-  if (a){
-                    a.href += ArtworkPageArray.id;
+    if (a) {
+        a.href += ArtworkPageArray.id;
 
-        }
+    }
 
-        document.querySelector(".artworkwrapper").appendChild(copy);
+    document.querySelector(".artworkwrapper").appendChild(copy);
 
 
 
 }
 
 
-function showSingleArtPage(art){
-        console.log(art, "art");
-        console.log(window.location);
-        const template = document.querySelector("template").content;
+function showSingleArtPage(art) {
+    console.log(art, "art");
+    console.log(window.location);
+    const template = document.querySelector("template").content;
 
-        const copy = template.cloneNode(true);
+    const copy = template.cloneNode(true);
 
-        const divArtDescription = copy.querySelector('#art-description');
-        console.log(divArtDescription, "div");
+    const divArtDescription = copy.querySelector('#art-description');
+    console.log(divArtDescription, "div");
 
 
     copy.querySelector('.title').textContent = art.title.rendered;
@@ -127,35 +125,53 @@ function showSingleArtPage(art){
     copy.querySelector(".area").textContent = art.place_year;
     copy.querySelector(".topimg").src = art._embedded["wp:featuredmedia"][0].source_url;
     copy.querySelector(".long-description").textContent = art.long_description;
-   if(art.portrait_one.guid){
-    copy.querySelector(".portrait1").src = art.portrait_one.guid;}
 
-    if(art.portrait_two.guid){
-    copy.querySelector(".portrait2").src = art.portrait_two.guid;}
+    if (art.portrait_one){
+        copy.querySelector(".portrait1").src = art.portrait_one.guid;
+    } else{
+        copy.querySelector(".portrait1").style.display = "none";
+    }
 
-    if(art.portrait_three === true){
-    copy.querySelector(".portrait3").src = art.portrait_three.guid;}
-     if(art.portrait_four.guid === 1){
-    copy.querySelector(".portrait4").src = art.portrait_four.guid;}
-    if(art.landscape_one.guid){
-    copy.querySelector(".landscape").src = art.landscape_one.guid;}
+    if (art.portrait_two){
+    copy.querySelector(".portrait2").src = art.portrait_two.guid;
+        } else{
+            copy.querySelector(".portrait2").style.display = "none";
+        }
+
+
+    if (art.portrait_three){
+    copy.querySelector(".portrait3").src = art.portrait_three.guid;
+        } else{
+            copy.querySelector(".portrait3").style.display = "none";
+        }
+
+
+    if (art.portrait_four){
+    copy.querySelector(".portrait4").src = art.portrait_four.guid;
+        } else{
+            copy.querySelector(".portrait4").style.display = "none";
+        }
+
+    if (art.landscape_one){
+    copy.querySelector(".landscape").src = art.landscape_one.guid;
+        }else{
+            copy.querySelector(".landscape").style.display = "none";
+        }
 
     const imgGalleryParent = copy.querySelector(".img-gallery");
 
-    if(art.gallery_pictures){
-    art.gallery_pictures.forEach(picture =>{
-        const currentImg = document.createElement("img");
-        currentImg.src = picture.guid;
-                console.log(currentImg, "in gallery pictures loop");
+    if (art.gallery_pictures) {
+        art.gallery_pictures.forEach(picture => {
+            const currentImg = document.createElement("img");
+            currentImg.src = picture.guid;
+            console.log(currentImg, "in gallery pictures loop");
 
-    imgGalleryParent.appendChild(currentImg);
+            imgGalleryParent.appendChild(currentImg);
 
-    })}
+        })
+    }
 
-
-
-        document.querySelector("#single-art").appendChild(copy);
-
-    };
+ document.querySelector("#single-art").appendChild(copy);
 
 
+};
