07c5a24d411a2494de60fd9fc66da4aa89bc02da
suppmen <suppmen@gmail.com>
2020-06-04 15:54:15 +0200

    adding scrolling to all pages

--- JS/about.js
+++ JS/about.js
@@ -31,3 +31,23 @@ function showCv() {
 
 
 }
+
+
+
+scroll = document.getElementById("scroll");
+window.onscroll = function () {
+    scrollDisplay()
+};
+
+function scrollDisplay() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scroll.style.display = "block";
+    } else {
+        scroll.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
